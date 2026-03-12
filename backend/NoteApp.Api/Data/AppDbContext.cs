dc2a65443a570e7de032547522dfe1f6c7ac7395
Belal Hashem <belal.hashem.saleh@gmail.com>
2026-03-12 04:42:10 +0200

    Migrate Note Id to GUID and set default CreatedAt
    - Change Note.Id from int to Guid with NEWSEQUENTIALID() default
    - Set CreatedAt to default to GETUTCDATE() in the database
    - Override OnModelCreating to enforce these defaults
    - Add EF Core migrations for schema changes

--- backend/NoteApp.Api/Data/AppDbContext.cs
+++ backend/NoteApp.Api/Data/AppDbContext.cs
@@ -10,6 +10,19 @@ namespace NoteApp.Api.Data
             
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //note constraints
+            modelBuilder.Entity<Note>()
+                .Property(n => n.Id)
+                .HasDefaultValueSql("NEWSEQUENTIALID()");
+            modelBuilder.Entity<Note>()
+                .Property(n => n.CreatedAt)
+                .HasDefaultValueSql("GETUTCDATE()");
+        }
+
         public DbSet<Note> Notes { get; set; }
     }
 }
--- backend/NoteApp.Api/Data/Migrations/20260312022602_ChangingIdFromIntToGUID.Designer.cs
+++ backend/NoteApp.Api/Data/Migrations/20260312022602_ChangingIdFromIntToGUID.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NoteApp.Api.Data;
 
@@ -11,9 +12,11 @@ using NoteApp.Api.Data;
 namespace NoteApp.Api.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260312022602_ChangingIdFromIntToGUID")]
+    partial class ChangingIdFromIntToGUID
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -34,9 +37,7 @@ namespace NoteApp.Api.Data.Migrations
                         .HasColumnType("nvarchar(max)");
 
                     b.Property<DateTime>("CreatedAt")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("datetime2")
-                        .HasDefaultValueSql("GETUTCDATE()");
+                        .HasColumnType("datetime2");
 
                     b.Property<string>("Title")
                         .IsRequired()
