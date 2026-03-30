99a9e6c71ca68810953e55c83f008ef846faeaf2
Belal Hashem <belal.hashem.saleh@gmail.com>
2026-03-30 14:59:54 +0200

    feat: migrate editor from Lexical to TipTap
    - Replace Lexical with TipTap rich text editor
    - Add new React hooks for editor functionality
    - Update dependencies and add TipTap utilities
    - Add SCSS module declarations

--- frontend/views/Editor/index.tsx
+++ frontend/views/Editor/index.tsx
@@ -1,50 +1,5 @@
 "use client";
-import { LexicalComposer } from "@lexical/react/LexicalComposer";
-import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
-import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
-import { ContentEditable } from "@lexical/react/LexicalContentEditable";
-import { LexicalErrorBoundary } from "@lexical/react/LexicalErrorBoundary";
-import { AutoFocusPlugin } from "@lexical/react/LexicalAutoFocusPlugin";
-import { OnChangePlugin } from "@lexical/react/LexicalOnChangePlugin";
-import { EditorState } from "lexical";
-
-const theme = {
-  // Theme styling goes here
-  //...
-};
-
-function onError(error: Error) {
-  console.error(error);
-}
-
+import Tiptap from "@/components/tiptap/TipTap";
 export default function Editor() {
-  const initialConfig = {
-    namespace: "MyEditor",
-    theme,
-    onError,
-  };
-
-  const onChange = (editorState: EditorState) => {
-    console.log(editorState.toJSON());
-  };
-
-  return (
-    <LexicalComposer initialConfig={initialConfig}>
-      <RichTextPlugin
-        contentEditable={
-          <ContentEditable
-            aria-placeholder={"Enter some text..."}
-            placeholder={
-              <div className="absolute top-18 left-4">Enter some text...</div>
-            }
-            className="grow px-4 py-2"
-          />
-        }
-        ErrorBoundary={LexicalErrorBoundary}
-      />
-      <OnChangePlugin onChange={onChange} />
-      <HistoryPlugin />
-      <AutoFocusPlugin />
-    </LexicalComposer>
-  );
+  return <Tiptap />;
 }
